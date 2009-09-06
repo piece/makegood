e31eb7f1f973b980b0ead29345fef3fab103de3f
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-09-06 12:29:43 +0900

    Changed StagehandTestRunnerLaunchShortcut class to use PDT as a PHP execution environment.

--- com.piece_framework.stagehand_testrunner/src/com/piece_framework/stagehand_testrunner/StagehandTestRunnerLaunchShortcut.java
+++ com.piece_framework.stagehand_testrunner/src/com/piece_framework/stagehand_testrunner/StagehandTestRunnerLaunchShortcut.java
@@ -1,20 +1,6 @@
 package com.piece_framework.stagehand_testrunner;
 
-import org.eclipse.debug.ui.ILaunchShortcut;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IEditorPart;
+import org.eclipse.php.internal.debug.ui.launching.PHPExeLaunchShortcut;
 
-public class StagehandTestRunnerLaunchShortcut implements ILaunchShortcut {
-
-    @Override
-    public void launch(ISelection selection, String mode) {
-        System.out.println(selection);
-        System.out.println(mode);
-    }
-
-    @Override
-    public void launch(IEditorPart editor, String mode) {
-        System.out.println(editor);
-        System.out.println(mode);
-    }
+public class StagehandTestRunnerLaunchShortcut extends PHPExeLaunchShortcut {
 }
