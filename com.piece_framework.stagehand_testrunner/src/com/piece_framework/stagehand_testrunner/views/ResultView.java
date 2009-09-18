b9cbe7ed79f40800ee5cd797122be5f57f10ddfa
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-09-18 20:06:25 +0900

    Added a result view and the icon.

---
+++ com.piece_framework.stagehand_testrunner/src/com/piece_framework/stagehand_testrunner/views/ResultView.java
@@ -0,0 +1,24 @@
+package com.piece_framework.stagehand_testrunner.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ResultView extends ViewPart {
+
+    public ResultView() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void createPartControl(Composite parent) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
