dafe87186fa0628158eb9927143f8af9f759d03c
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-09-24 17:36:12 +0900

    Fixed imported classes.

--- com.piece_framework.makegood.launch.test/tests/com/piece_framework/makegood/launch/phpunit/TestResultConverterTest.java
+++ com.piece_framework.makegood.launch.test/tests/com/piece_framework/makegood/launch/phpunit/TestResultConverterTest.java
@@ -1,22 +1,13 @@
 package com.piece_framework.makegood.launch.phpunit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 public class TestResultConverterTest {
 
