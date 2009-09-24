ede6ecff636d0e786ea99ddce811cf719065d5bb
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-09-24 17:11:34 +0900

    Changed the scope of property.

--- com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/phpunit/Failure.java
+++ com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/phpunit/Failure.java
@@ -1,22 +1,14 @@
 package com.piece_framework.makegood.launch.phpunit;
 
 public class Failure {
-    private String type;
-    private String content;
+    String type;
+    String content;
 
     public String getType() {
         return type;
     }
 
-    void setType(String type) {
-        this.type = type;
-    }
-
     public String getContent() {
         return content;
-    }
-
-    void setContent(String content) {
-        this.content = content;
     }
 }
--- com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/phpunit/TestCase.java
+++ com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/phpunit/TestCase.java
@@ -1,58 +1,34 @@
 package com.piece_framework.makegood.launch.phpunit;
 
 public class TestCase extends TestResult {
-    private String file;
-    private String className;
-    private int assertionCount;
-    private int line;
-    private double time;
-    private Failure failure;
+    String file;
+    String className;
+    int assertionCount;
+    int line;
+    double time;
+    Failure failure;
 
     public String getFile() {
         return file;
-    }
-
-    void setFile(String file) {
-        this.file = file;
     }
 
     public String getClassName() {
         return className;
     }
 
-    void setClassName(String className) {
-        this.className = className;
-    }
-
     public int getAssertionCount() {
         return assertionCount;
-    }
-
-    void setAssertionCount(int assertionCount) {
-        this.assertionCount = assertionCount;
     }
 
     public int getLine() {
         return line;
     }
 
-    void setLine(int line) {
-        this.line = line;
-    }
-
     public double getTime() {
         return time;
     }
 
-    void setTime(double time) {
-        this.time = time;
-    }
-
     public Failure getFailure() {
         return failure;
-    }
-
-    void setFailure(Failure failure) {
-        this.failure = failure;
     }
 }
