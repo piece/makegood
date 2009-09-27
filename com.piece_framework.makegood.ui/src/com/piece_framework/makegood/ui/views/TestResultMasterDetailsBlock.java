62bd34d61270d331e218b3cfdda0b74ba60caba8
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-09-27 12:27:11 +0900

    Added TestResultMasterDetailsBlock class.

---
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultMasterDetailsBlock.java
@@ -0,0 +1,41 @@
+package com.piece_framework.makegood.ui.views;
+
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.forms.DetailsPart;
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.MasterDetailsBlock;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Section;
+
+public class TestResultMasterDetailsBlock extends MasterDetailsBlock {
+    @Override
+    protected void createMasterPart(IManagedForm managedForm, Composite parent) {
+        FormToolkit toolkit = managedForm.getToolkit();
+        Section section = toolkit.createSection(parent, Section.NO_TITLE);
+
+        Composite client = toolkit.createComposite(section);
+        client.setLayout(new FillLayout());
+        Tree tree = toolkit.createTree(client, SWT.BORDER);
+        TreeViewer viewer = new TreeViewer(tree);
+        viewer.setContentProvider(new TestResultContentProvider());
+        viewer.setLabelProvider(new TestResultLabelProvider());
+
+        section.setClient(client);
+    }
+
+    @Override
+    protected void createToolBarActions(IManagedForm managedForm) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void registerPages(DetailsPart detailsPart) {
+        // TODO Auto-generated method stub
+
+    }
+}
