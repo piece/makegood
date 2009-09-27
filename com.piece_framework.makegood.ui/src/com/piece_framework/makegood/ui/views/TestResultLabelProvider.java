7ab81d818e364188d37a1ebbcad52b732a783a52
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-09-27 13:18:28 +0900

    Added getImage() to show the dummy icons.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultLabelProvider.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultLabelProvider.java
@@ -1,9 +1,14 @@
 package com.piece_framework.makegood.ui.views;
 
 import org.eclipse.jface.viewers.LabelProvider;
-import com.piece_framework.makegood.launch.phpunit.TestResult;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+
 import com.piece_framework.makegood.launch.phpunit.Error;
 import com.piece_framework.makegood.launch.phpunit.Failure;
+import com.piece_framework.makegood.launch.phpunit.TestCase;
+import com.piece_framework.makegood.launch.phpunit.TestResult;
 
 public class TestResultLabelProvider extends LabelProvider {
     @Override
@@ -19,5 +24,21 @@ public class TestResultLabelProvider extends LabelProvider {
             return failure.getType();
         }
         return super.getText(element);
+    }
+
+    @Override
+    public Image getImage(Object element) {
+        ISharedImages images = PlatformUI.getWorkbench().getSharedImages();
+        if (element instanceof TestCase) {
+            TestCase testCase = (TestCase) element;
+            if (testCase.getError() != null) {
+                return images.getImage(ISharedImages.IMG_OBJS_ERROR_TSK);
+            } else if (testCase.getFailure() != null){
+                return images.getImage(ISharedImages.IMG_OBJS_WARN_TSK);
+            } else {
+                return images.getImage(ISharedImages.IMG_OBJS_INFO_TSK);
+            }
+        }
+        return super.getImage(element);
     }
 }
