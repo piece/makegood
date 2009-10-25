0448b16ba1bbc9a17c5b239d957dd751d11c7379
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-10-26 01:15:13 +0900

    Improved earlyStartup().
    Added the weaving code to use the dummy resource for the PHP configuration.
    Improved the weaving code to use transformIncludePaths().

--- com.piece_framework.makegood.aspect.include_path_protection/src/com/piece_framework/makegood/aspect/include_path_protection/Startup.java
+++ com.piece_framework.makegood.aspect.include_path_protection/src/com/piece_framework/makegood/aspect/include_path_protection/Startup.java
@@ -9,6 +9,7 @@ import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.NotFoundException;
+import javassist.expr.Cast;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
@@ -22,7 +23,9 @@ public class Startup implements IStartup {
 
         String[] requiredBundles = {"org.eclipse.php.debug.core",
                                     "com.piece_framework.makegood.aspect.include_path_protection",
-                                    "org.eclipse.core.resources"
+                                    "org.eclipse.core.resources",
+                                    "org.eclipse.equinox.common",
+                                    "org.eclipse.php.core"
                                     };
         for (String requiredBundle: requiredBundles) {
             try {
@@ -52,16 +55,44 @@ public class Startup implements IStartup {
             CtClass targetClass = pool.get("org.eclipse.php.internal.debug.core.phpIni.PHPINIUtil");
             CtMethod targetMethod = targetClass.getDeclaredMethod("createPhpIniByProject");
             targetMethod.instrument(new ExprEditor() {
+                public void edit(Cast cast) throws CannotCompileException {
+                    try {
+                        CtClass castClass = cast.getType();
+                        if (castClass.getName().equals("org.eclipse.core.resources.IContainer")) {
+                            cast.replace(
+"$_ = null;" +
+"if (pathObject.getEntry() instanceof org.eclipse.core.resources.IContainer) {" +
+"    $_ = ($r) pathObject.getEntry();" +
+"} else {" +
+"    org.eclipse.core.resources.IResource resource = (org.eclipse.core.resources.IResource) pathObject.getEntry();" +
+"    includePath.add(resource.getFullPath().toOSString());" +
+"}"
+                                );
+                        }
+                    } catch (NotFoundException e) {
+                    }
+                }
+
                 public void edit(MethodCall methodCall) throws CannotCompileException {
-                    if (methodCall.getMethodName().equals("modifyIncludePath")) {
+                    if (methodCall.getClassName().equals("org.eclipse.core.resources.IContainer")
+                        && methodCall.getMethodName().equals("getLocation")
+                        ) {
                         methodCall.replace(
-"com.piece_framework.makegood.aspect.include_path_protection.PHPConfiguration configuration =" +
+"$_ = null;" +
+"if (container != null) {" +
+"    $_ = $proceed($$);" +
+"}"
+                            );
+                    } else if (methodCall.getMethodName().equals("modifyIncludePath")) {
+                        methodCall.replace(
+"com.piece_framework.makegood.aspect.include_path_protection.PHPConfiguration phpConfiguration =" +
 "    new com.piece_framework.makegood.aspect.include_path_protection.PHPConfiguration();" +
-"String[] includePathsFromConfiguration = configuration.combineIncludePaths($1," +
-"                                                                           includePath,"+
-"                                                                           project" +
-"                                                                           );" +
-"$_ = $proceed($1, includePathsFromConfiguration);");
+"String[] transformedIncludePaths = phpConfiguration.transformIncludePaths($1," +
+"                                                                          includePath,"+
+"                                                                          project" +
+"                                                                          );" +
+"$_ = $proceed($1, transformedIncludePaths);"
+                            );
                     }
                 }
             });
