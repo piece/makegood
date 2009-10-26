e9b183444f494890697eacdff5df88a855b3e521
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-10-26 14:34:07 +0900

    Changed earlyStartup() to use BundleLoader class.

--- com.piece_framework.makegood.aspect.include_path_settings/src/com/piece_framework/makegood/aspect/include_path_settings/Startup.java
+++ com.piece_framework.makegood.aspect.include_path_settings/src/com/piece_framework/makegood/aspect/include_path_settings/Startup.java
@@ -1,8 +1,4 @@
 package com.piece_framework.makegood.aspect.include_path_settings;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -12,48 +8,42 @@ import javassist.NotFoundException;
 import javassist.expr.ExprEditor;
 import javassist.expr.NewExpr;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.IStartup;
+import org.osgi.framework.Bundle;
+
+import com.piece_framework.makegood.javassist.BundleLoader;
 
 
 public class Startup implements IStartup {
     @Override
     public void earlyStartup() {
-        ClassPool pool = ClassPool.getDefault();
-
-        String[] requiredBundles = {"org.eclipse.php.ui",
-                                    "com.piece_framework.makegood.aspect.include_path_settings",
-                                    "com.piece_framework.makegood.include_path",
-                                    "org.eclipse.dltk.ui",
-                                    "org.eclipse.core.resources",
-                                    "org.eclipse.jface"
-                                    };
-        for (String requiredBundle: requiredBundles) {
-            try {
-                URL bundleURL = new URL(Platform.getBundle(requiredBundle).getLocation());
-                String bundleLocation = null;
-                if (bundleURL.getFile().startsWith("file:")) {
-                    bundleLocation = bundleURL.getFile().substring("file:".length());
-                } else {
-                    bundleLocation = bundleURL.getFile();
-                }
-
-                if (new File(bundleLocation).isDirectory()) {
-                    bundleLocation += "bin";
-                }
-
-                pool.appendClassPath(bundleLocation);
-            } catch (MalformedURLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (NotFoundException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+        BundleLoader loader = new BundleLoader(
+                new String[]{"org.eclipse.php.ui",
+                             "com.piece_framework.makegood.aspect.include_path_settings",
+                             "com.piece_framework.makegood.include_path",
+                             "org.eclipse.dltk.ui",
+                             "org.eclipse.core.resources",
+                             "org.eclipse.jface"
+                             });
+        try {
+            loader.load();
+        } catch (NotFoundException e) {
+            IStatus status = new Status(IStatus.ERROR,
+                                        "com.piece_framework.makegood.aspect.include_path_settings",
+                                        0,
+                                        e.getMessage(),
+                                        e
+                                        );
+            Bundle bundle = Platform.getBundle("com.piece_framework.makegood.aspect.include_path_settings");
+            Platform.getLog(bundle).log(status);
+            return;
         }
 
         try {
-            CtClass targetClass = pool.get("org.eclipse.php.internal.ui.preferences.includepath.PHPIPListLabelProvider");
+            CtClass targetClass = ClassPool.getDefault().get("org.eclipse.php.internal.ui.preferences.includepath.PHPIPListLabelProvider");
             CtMethod newMethod = CtNewMethod.make(
 "public String getCPListElementText(org.eclipse.dltk.internal.ui.wizards.buildpath.BPListElement cpentry) {" +
 "    org.eclipse.core.resources.IResource target = cpentry.getResource();" +
@@ -86,7 +76,7 @@ public class Startup implements IStartup {
         }
 
         try {
-            CtClass targetClass = pool.get("org.eclipse.php.internal.ui.preferences.includepath.PHPIncludePathsBlock");
+            CtClass targetClass = ClassPool.getDefault().get("org.eclipse.php.internal.ui.preferences.includepath.PHPIncludePathsBlock");
             CtMethod targetMethod = targetClass.getDeclaredMethod("createControl");
             targetMethod.instrument(new ExprEditor() {
                 public void edit(NewExpr expression) throws CannotCompileException {
