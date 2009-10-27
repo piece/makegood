7165bdb7b26ff5cc5357fffc41b5ad950b610d63
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-10-27 20:23:46 +0900

    Added the class RunTestFromExplorer to run tests which are included in the test file.

---
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunTestFromEditorInFile.java
@@ -0,0 +1,10 @@
+package com.piece_framework.makegood.ui.handler;
+
+import com.piece_framework.makegood.ui.launch.MakeGoodLaunchShortcut;
+
+public class RunTestFromEditorInFile extends RunTestFromEditor {
+    @Override
+    protected int getRunLevel() {
+        return MakeGoodLaunchShortcut.RUN_TESTS_ON_FILE;
+    }
+}
