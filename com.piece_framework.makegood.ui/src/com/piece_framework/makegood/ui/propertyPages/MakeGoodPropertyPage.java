7d0f09bb9ee741bdb46c850c722ae8232b9262b6
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-10-28 15:44:52 +0900

    Changed the class MakeGoodPropertyPage to store the preload script in the project property.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/propertyPages/MakeGoodPropertyPage.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/propertyPages/MakeGoodPropertyPage.java
@@ -5,7 +5,9 @@ import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.jface.viewers.Viewer;
@@ -27,11 +29,13 @@ import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
 import org.eclipse.ui.dialogs.PropertyPage;
 import org.eclipse.ui.model.WorkbenchContentProvider;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
+import org.osgi.service.prefs.BackingStoreException;
 
 import com.piece_framework.makegood.ui.Activator;
 
 public class MakeGoodPropertyPage extends PropertyPage implements IWorkbenchPropertyPage {
     private static String PRELOAD_SCRIP_KEY = "preload_script";
+    private Text preloadScript;
 
     @Override
     protected Control createContents(Composite parent) {
@@ -44,7 +48,7 @@ public class MakeGoodPropertyPage extends PropertyPage implements IWorkbenchProp
         Label label = new Label(composite, SWT.NONE);
         label.setText("&Preload Script:");
 
-        final Text preloadScript = new Text(composite, SWT.SINGLE | SWT.BORDER);
+        preloadScript = new Text(composite, SWT.SINGLE | SWT.BORDER);
         preloadScript.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
         Button browse = new Button(composite, SWT.NONE);
@@ -110,6 +114,23 @@ public class MakeGoodPropertyPage extends PropertyPage implements IWorkbenchProp
         preloadScript.setText(context.getNode(Activator.PLUGIN_ID).get(PRELOAD_SCRIP_KEY, ""));
 
         return composite;
+    }
+
+    @Override
+    public boolean performOk() {
+        IEclipsePreferences projectNode = new ProjectScope(getProject()).getNode(Activator.PLUGIN_ID);
+        projectNode.put(PRELOAD_SCRIP_KEY, preloadScript.getText());
+        try {
+            projectNode.flush();
+        } catch (BackingStoreException e) {
+            Activator.getDefault().getLog().log(new Status(Status.ERROR,
+                                                           Activator.PLUGIN_ID,
+                                                           e.getMessage(),
+                                                           e
+                                                           ));
+        }
+
+        return true;
     }
 
     private IProject getProject() {
