6a1d1243e3b70e67935affbc0b27523656aacf9e
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-10-28 10:29:59 +0900

    Added the class MakeGoodPropertyPage to show the MakeGood property page in a project property.

---
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/propertyPages/MakeGoodPropertyPage.java
@@ -0,0 +1,13 @@
+package com.piece_framework.makegood.ui.propertyPages;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class MakeGoodPropertyPage extends PropertyPage implements IWorkbenchPropertyPage {
+    @Override
+    protected Control createContents(Composite parent) {
+        return null;
+    }
+}
