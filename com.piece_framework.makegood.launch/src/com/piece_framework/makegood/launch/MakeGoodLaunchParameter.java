ef43435deb7e678fec684544f0c98ecff7ed11ad
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-10-29 22:34:26 +0900

    Added getScript() to return the script which is from the target object.

--- com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/MakeGoodLaunchParameter.java
+++ com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/MakeGoodLaunchParameter.java
@@ -1,5 +1,15 @@
 package com.piece_framework.makegood.launch;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.IProjectFragment;
+import org.eclipse.dltk.core.IScriptFolder;
+
+import com.piece_framework.makegood.core.PHPResource;
+
 public class MakeGoodLaunchParameter {
     private static MakeGoodLaunchParameter parameter;
     private Object target;
@@ -20,5 +30,55 @@ public class MakeGoodLaunchParameter {
 
     public Object getTarget() {
         return target;
+    }
+
+    public String getScript() {
+        boolean isFolder = target instanceof IProjectFragment
+                           || target instanceof IScriptFolder
+                           || target instanceof IFolder;
+        boolean isFile = target instanceof IFile;
+        boolean isModelElement = target instanceof IModelElement;
+
+        IFile file = null;
+        if (isFolder) {
+            IFolder folder = null;
+            if (target instanceof IModelElement) {
+                folder = (IFolder) ((IModelElement) target).getResource();
+            } else if (target instanceof IFolder) {
+                folder = (IFolder) target;
+            }
+            file = findDummyFile(folder);
+        } else if (isFile) {
+            file = (IFile) target;
+        } else if (isModelElement) {
+            file = (IFile) ((IModelElement) target).getResource();
+        }
+
+        if (file != null) {
+            return file.getFullPath().toString();
+        }
+        return null;
+    }
+
+    private IFile findDummyFile(IFolder folder) {
+        try {
+            for (IResource resource: folder.members()) {
+                if (PHPResource.isTrue(resource)) {
+                    return (IFile) resource;
+                }
+            }
+            for (IResource resource: folder.members()) {
+                if (resource instanceof IFolder) {
+                    IFile file = findDummyFile((IFolder) resource);
+                    if (file != null) {
+                        return file;
+                    }
+                }
+            }
+        } catch (CoreException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 }
