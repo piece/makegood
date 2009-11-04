2b2b16d5f524ba052f5ba4535f1d45e40269665b
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-04 12:29:17 +0900

    Changed the createPartControl() to show the progress bar.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultView.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultView.java
@@ -1,29 +1,15 @@
 package com.piece_framework.makegood.ui.views;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.List;
-
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.IDebugEventSetListener;
-import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.php.internal.debug.core.model.IPHPDebugTarget;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.forms.ManagedForm;
-import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.part.ViewPart;
-import org.eclipse.ui.progress.UIJob;
-
-import com.piece_framework.makegood.launch.phpunit.TestResultConverter;
-import com.piece_framework.makegood.launch.phpunit.TestSuite;
 
 public class TestResultView extends ViewPart {
     private ScrolledForm root;
@@ -35,59 +21,68 @@ public class TestResultView extends ViewPart {
 
     @Override
     public void createPartControl(Composite parent) {
-        FormToolkit toolkit = new FormToolkit(parent.getDisplay());
-        root = toolkit.createScrolledForm(parent);
+        parent.setLayout(new GridLayout(1, false));
 
-        final TestResultMasterDetailsBlock block = new TestResultMasterDetailsBlock();
-        block.createContent(new ManagedForm(toolkit, root));
+        Label progressBar = new Label(parent, SWT.BORDER);
+        progressBar.setBackground(new Color(parent.getDisplay(), 95, 191, 95));
+        GridData horizontalFillGrid = new GridData();
+        horizontalFillGrid.horizontalAlignment = GridData.FILL;
+        horizontalFillGrid.grabExcessHorizontalSpace = true;
+        progressBar.setLayoutData(horizontalFillGrid);
 
-        listener = new IDebugEventSetListener() {
-            @Override
-            public void handleDebugEvents(DebugEvent[] events) {
-                for (DebugEvent event: events) {
-                    if (event.getKind() == DebugEvent.TERMINATE
-                        && event.getSource() instanceof IPHPDebugTarget
-                        ) {
-                        final IPHPDebugTarget debugTarget = (IPHPDebugTarget) event.getSource();
+//        FormToolkit toolkit = new FormToolkit(parent.getDisplay());
+//        root = toolkit.createScrolledForm(parent);
 
-                        Job job = new UIJob("MakeGood result parse") {
-                            public IStatus runInUIThread(IProgressMonitor monitor) {
-                                ILaunchConfiguration configuration = debugTarget.getLaunch().getLaunchConfiguration();
-
-                                try {
-                                    String logFile = configuration.getAttribute("LOG_JUNIT", (String) null);
-                                    List<TestSuite> suites = TestResultConverter.convert(new File(logFile));
-                                    block.setInput(suites);
-                                } catch (CoreException e) {
-                                    // TODO Auto-generated catch block
-                                    e.printStackTrace();
-                                } catch (FileNotFoundException e) {
-                                    // TODO Auto-generated catch block
-                                    e.printStackTrace();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-
-                                String[] results = debugTarget.getOutputBuffer().toString().split("\n");
-                                int lastLine = results.length - 1;
-                                String result = results[lastLine];
-
-                                root.setText(result);
-                                if (result.startsWith("OK")) {
-                                    root.setBackground(new Color(null, 0, 255, 0));
-                                } else {
-                                    root.setBackground(new Color(null, 255, 0, 0));
-                                }
-
-                                return Status.OK_STATUS;
-                            }
-                        };
-                        job.schedule();
-                    }
-                }
-            }
-        };
-        DebugPlugin.getDefault().addDebugEventListener(listener);
+//        final TestResultMasterDetailsBlock block = new TestResultMasterDetailsBlock();
+//        block.createContent(new ManagedForm(toolkit, root));
+//
+//        listener = new IDebugEventSetListener() {
+//            @Override
+//            public void handleDebugEvents(DebugEvent[] events) {
+//                for (DebugEvent event: events) {
+//                    if (event.getKind() == DebugEvent.TERMINATE
+//                        && event.getSource() instanceof IPHPDebugTarget
+//                        ) {
+//                        final IPHPDebugTarget debugTarget = (IPHPDebugTarget) event.getSource();
+//
+//                        Job job = new UIJob("MakeGood result parse") {
+//                            public IStatus runInUIThread(IProgressMonitor monitor) {
+//                                ILaunchConfiguration configuration = debugTarget.getLaunch().getLaunchConfiguration();
+//
+//                                try {
+//                                    String logFile = configuration.getAttribute("LOG_JUNIT", (String) null);
+//                                    List<TestSuite> suites = TestResultConverter.convert(new File(logFile));
+//                                    block.setInput(suites);
+//                                } catch (CoreException e) {
+//                                    // TODO Auto-generated catch block
+//                                    e.printStackTrace();
+//                                } catch (FileNotFoundException e) {
+//                                    // TODO Auto-generated catch block
+//                                    e.printStackTrace();
+//                                } catch (Exception e) {
+//                                    e.printStackTrace();
+//                                }
+//
+//                                String[] results = debugTarget.getOutputBuffer().toString().split("\n");
+//                                int lastLine = results.length - 1;
+//                                String result = results[lastLine];
+//
+//                                root.setText(result);
+//                                if (result.startsWith("OK")) {
+//                                    root.setBackground(new Color(null, 0, 255, 0));
+//                                } else {
+//                                    root.setBackground(new Color(null, 255, 0, 0));
+//                                }
+//
+//                                return Status.OK_STATUS;
+//                            }
+//                        };
+//                        job.schedule();
+//                    }
+//                }
+//            }
+//        };
+//        DebugPlugin.getDefault().addDebugEventListener(listener);
     }
 
     @Override
