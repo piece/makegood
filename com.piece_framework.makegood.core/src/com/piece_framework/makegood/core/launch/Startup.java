fad14c97db2fcb7ce4b9793e4a86bcc85e7e0e53
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-06 11:15:58 +0900

    Extended the extension point org.eclipse.ui.startup.

---
+++ com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/launch/Startup.java
@@ -0,0 +1,10 @@
+package com.piece_framework.makegood.core.launch;
+
+import org.eclipse.ui.IStartup;
+
+public class Startup implements IStartup {
+    @Override
+    public void earlyStartup() {
+        // TODO Auto-generated method stub
+    }
+}
