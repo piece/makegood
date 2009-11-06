88666dfaaa50d76ceefe9fc1522b27a498b4d46a
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-06 14:07:56 +0900

    Changed terminate() to show the test result.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultViewSetter.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultViewSetter.java
@@ -1,8 +1,23 @@
 package com.piece_framework.makegood.ui.views;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.ILaunch;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.progress.UIJob;
 
 import com.piece_framework.makegood.core.launch.IMakeGoodEventListener;
+import com.piece_framework.makegood.launch.MakeGoodViewRegistry;
+import com.piece_framework.makegood.launch.phpunit.TestResultConverter;
+import com.piece_framework.makegood.launch.phpunit.TestSuite;
 
 public class TestResultViewSetter implements IMakeGoodEventListener {
     @Override
@@ -12,6 +27,38 @@ public class TestResultViewSetter implements IMakeGoodEventListener {
 
     @Override
     public void terminate(ILaunch launch) {
-        System.out.println("terminate:" + launch);
+        String log = null;
+        try {
+            log = launch.getLaunchConfiguration().getAttribute("LOG_JUNIT", (String) null);
+        } catch (CoreException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (log == null) {
+            // TODO
+            return;
+        }
+        final File logFile = new File(log);
+
+        Job job = new UIJob("MakeGood result parse") {
+            @Override
+            public IStatus runInUIThread(IProgressMonitor monitor) {
+                IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+                IViewPart view = page.findView(MakeGoodViewRegistry.getViewId());
+                if (!(view instanceof TestResultView)) {
+                    return Status.OK_STATUS;
+                }
+
+                try {
+                    java.util.List<TestSuite> suites = TestResultConverter.convert(logFile);
+                    ((TestResultView) view).showTestResult(suites);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                return Status.OK_STATUS;
+            }
+        };
+        job.schedule();
     }
 }
