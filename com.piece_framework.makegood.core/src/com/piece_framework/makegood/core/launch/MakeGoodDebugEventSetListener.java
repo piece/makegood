96fa3e63f27cd2d45af4ca4ae085a3b079f0fc32
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-06 11:12:19 +0900

    Added the class MakeGoodDebugEventSetListener to listen debug events.

---
+++ com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/launch/MakeGoodDebugEventSetListener.java
@@ -0,0 +1,12 @@
+package com.piece_framework.makegood.core.launch;
+
+import org.eclipse.debug.core.DebugEvent;
+import org.eclipse.debug.core.IDebugEventSetListener;
+
+public class MakeGoodDebugEventSetListener implements IDebugEventSetListener {
+    @Override
+    public void handleDebugEvents(DebugEvent[] events) {
+        // TODO Auto-generated method stub
+
+    }
+}
