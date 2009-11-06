b4455a4246435b311d2863448dba096c312ec617
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-06 11:57:14 +0900

    Added constructor to recieve IMakeGoodEventListener list.

--- com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/launch/MakeGoodDebugEventSetListener.java
+++ com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/launch/MakeGoodDebugEventSetListener.java
@@ -1,9 +1,17 @@
 package com.piece_framework.makegood.core.launch;
 
+import java.util.List;
+
 import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.IDebugEventSetListener;
 
 public class MakeGoodDebugEventSetListener implements IDebugEventSetListener {
+    List<IMakeGoodEventListener> eventListeners;
+
+    public MakeGoodDebugEventSetListener(List<IMakeGoodEventListener> eventListeners) {
+        this.eventListeners = eventListeners;
+    }
+
     @Override
     public void handleDebugEvents(DebugEvent[] events) {
         // TODO Auto-generated method stub
