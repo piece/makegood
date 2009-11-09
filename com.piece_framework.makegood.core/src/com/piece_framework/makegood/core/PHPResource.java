433ef1eae59745e7cfdc7d8af3eed5c2ab7b04b2
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-10 06:56:05 +0900

    Added includeTestClass().

--- com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/PHPResource.java
+++ com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/PHPResource.java
@@ -4,6 +4,9 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.core.ModelException;
 
 public class PHPResource {
     public static String CONTENT_TYPE = "org.eclipse.php.core.phpsource";
@@ -15,5 +18,24 @@ public class PHPResource {
 
         IContentType contentType = Platform.getContentTypeManager().getContentType(CONTENT_TYPE);
         return contentType.isAssociatedWith(target.getName());
+    }
+
+    public static boolean includeTestClass(ISourceModule source) {
+        boolean isTestClass = false;
+        try {
+            for (IType type : source.getAllTypes()) {
+                for (String superClass : type.getSuperClasses()) {
+                    if (superClass.equals("PHPUnit_Framework_TestCase")) {
+                        isTestClass = true;
+                        break;
+                    }
+                }
+                if (isTestClass) {
+                    break;
+                }
+            }
+        } catch (ModelException e) {
+        }
+        return isTestClass;
     }
 }
