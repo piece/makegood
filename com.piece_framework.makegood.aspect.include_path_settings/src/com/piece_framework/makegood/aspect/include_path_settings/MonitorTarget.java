b3a6e30b3595f1545499756cb783bcebe3982fdf
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-11 21:55:52 +0900

    Extended the extension point com.piece_framework.makegood.javassist.monitorTarget.

---
+++ com.piece_framework.makegood.aspect.include_path_settings/src/com/piece_framework/makegood/aspect/include_path_settings/MonitorTarget.java
@@ -0,0 +1,12 @@
+package com.piece_framework.makegood.aspect.include_path_settings;
+
+import com.piece_framework.makegood.javassist.monitor.IMonitorTarget;
+
+public class MonitorTarget implements IMonitorTarget {
+    static boolean endWeaving;
+
+    @Override
+    public boolean endWeaving() {
+        return MonitorTarget.endWeaving;
+    }
+}
