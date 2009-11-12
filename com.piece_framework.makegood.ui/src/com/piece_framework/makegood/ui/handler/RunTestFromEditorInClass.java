be56bec48af5a2e3ba429ec91d63ae734390fee9
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-12 10:36:38 +0900

    Changed isEnabled() to be unvisible the class menu if a cursor is out of class.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunTestFromEditorInClass.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunTestFromEditorInClass.java
@@ -1,11 +1,50 @@
 package com.piece_framework.makegood.ui.handler;
 
+import org.eclipse.dltk.core.IMethod;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.core.ModelException;
+import org.eclipse.dltk.core.ScriptModelUtil;
+import org.eclipse.dltk.internal.ui.editor.EditorUtility;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.ITextEditor;
+
 import com.piece_framework.makegood.ui.launch.MakeGoodLaunchShortcut;
 
 public class RunTestFromEditorInClass extends RunTestFromEditor {
     @Override
     public boolean isEnabled() {
-        return super.isEnabled();
+        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        IEditorPart editor = page.getActiveEditor();
+
+        ISourceModule source = EditorUtility.getEditorInputModelElement(editor, false);
+        if (source == null) {
+            return false;
+        }
+
+        ITextEditor textEditor = (ITextEditor) editor;
+        ISelectionProvider provider = (ISelectionProvider) textEditor.getSelectionProvider();
+        ITextSelection selection = (ITextSelection) provider.getSelection();
+        int offset = selection.getOffset();
+
+        IModelElement element = null;
+        try {
+            ScriptModelUtil.reconcile(source);
+            element = source.getElementAt(offset);
+        } catch (ModelException e) {
+        }
+
+        if (element instanceof IType
+            || element instanceof IMethod
+            ) {
+            return true;
+        }
+        return false;
     }
 
     @Override
