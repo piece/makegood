35759d17133bbad4db0be5cfac046a03cf145596
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-12 16:12:33 +0900

    Changed isEnabled() to check isEnabled() of the super class.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunTestFromEditorInClass.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunTestFromEditorInClass.java
@@ -2,17 +2,7 @@ package com.piece_framework.makegood.ui.handler;
 
 import org.eclipse.dltk.core.IMethod;
 import org.eclipse.dltk.core.IModelElement;
-import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.IType;
-import org.eclipse.dltk.core.ModelException;
-import org.eclipse.dltk.core.ScriptModelUtil;
-import org.eclipse.dltk.internal.ui.editor.EditorUtility;
-import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.texteditor.ITextEditor;
 
 import com.piece_framework.makegood.ui.launch.MakeGoodLaunchShortcut;
 import com.piece_framework.makegood.ui.parser.EditorParser;
@@ -20,6 +10,10 @@ import com.piece_framework.makegood.ui.parser.EditorParser;
 public class RunTestFromEditorInClass extends RunTestFromEditor {
     @Override
     public boolean isEnabled() {
+        if (!super.isEnabled()) {
+            return false;
+        }
+
         EditorParser parser = new EditorParser();
         IModelElement element = parser.getModelElementOnSelection();
         if (element instanceof IType
