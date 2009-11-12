214c26e9f9205eb796dff6431974fa3c4e74904e
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-12 10:23:13 +0900

    Added isEnabled().

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunTestFromEditorInClass.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunTestFromEditorInClass.java
@@ -4,6 +4,11 @@ import com.piece_framework.makegood.ui.launch.MakeGoodLaunchShortcut;
 
 public class RunTestFromEditorInClass extends RunTestFromEditor {
     @Override
+    public boolean isEnabled() {
+        return super.isEnabled();
+    }
+
+    @Override
     protected int getRunLevel() {
         return MakeGoodLaunchShortcut.RUN_TESTS_ON_CLASS;
     }
