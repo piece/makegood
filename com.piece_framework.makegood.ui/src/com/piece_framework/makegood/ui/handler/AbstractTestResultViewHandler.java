c5401d55d3ca5b8178b0212be2d3b1a64721c30f
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-13 14:44:40 +0900

    Removed the class AbstractTestResultViewHandler.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/AbstractTestResultViewHandler.java
+++
@@ -1,6 +0,0 @@
-package com.piece_framework.makegood.ui.handler;
-
-import org.eclipse.core.commands.AbstractHandler;
-
-public abstract class AbstractTestResultViewHandler extends AbstractHandler {
-}
