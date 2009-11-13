f3da23235e815e60e13e33bafda6e4e771faa1cd
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-13 14:22:23 +0900

    Added the class NextResultHandler to go to next result on the tree.

---
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/NextResultHandler.java
@@ -0,0 +1,12 @@
+package com.piece_framework.makegood.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class NextResultHandler extends AbstractHandler {
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        return null;
+    }
+}
