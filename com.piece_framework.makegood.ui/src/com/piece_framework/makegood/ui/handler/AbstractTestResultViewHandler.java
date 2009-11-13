9cfb7103a5a1be79b2364c0e970217338ed6c124
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-13 13:48:56 +0900

    Added the class AbstractTestResultViewHandler.

---
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/AbstractTestResultViewHandler.java
@@ -0,0 +1,6 @@
+package com.piece_framework.makegood.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+
+public abstract class AbstractTestResultViewHandler extends AbstractHandler {
+}
