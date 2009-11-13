cf2c34c80562e06f2f61bdfb8850947d1424a439
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-13 10:12:59 +0900

    Changed execute() to run related tests.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunRelatedTestFromEditor.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handler/RunRelatedTestFromEditor.java
@@ -3,10 +3,18 @@ package com.piece_framework.makegood.ui.handler;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.piece_framework.makegood.ui.launch.MakeGoodLaunchShortcut;
 
 public class RunRelatedTestFromEditor extends AbstractHandler {
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        return null;
+        IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
+        MakeGoodLaunchShortcut shortcut = new MakeGoodLaunchShortcut();
+        shortcut.setRunLevelOnEditor(MakeGoodLaunchShortcut.RUN_RELATED_TESTS);
+        shortcut.launch(editorPart, "run");
+        return shortcut;
     }
 }
