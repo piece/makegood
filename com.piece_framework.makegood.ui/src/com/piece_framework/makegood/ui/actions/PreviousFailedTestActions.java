0e45ee33c23629e7858b0fc48d92c9b6cb75570c
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2009-11-14 10:05:52 +0900

    Changed the display method of toolbar to bind a keyboard shortcut.

---
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/actions/PreviousFailedTestActions.java
@@ -0,0 +1,23 @@
+package com.piece_framework.makegood.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+public class PreviousFailedTestActions implements IViewActionDelegate {
+    @Override
+    public void init(IViewPart view) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void run(IAction action) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void selectionChanged(IAction action, ISelection selection) {
+        // TODO Auto-generated method stub
+    }
+}
