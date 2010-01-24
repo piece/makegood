bb032f02be6a9e17ec909abbb901802960bfee64
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2010-01-25 00:09:34 +0900

    Changed the JUnit XML parser.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultViewSetter.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/TestResultViewSetter.java
@@ -2,6 +2,11 @@ package com.piece_framework.makegood.ui.views;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -11,18 +16,32 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IProcess;
-import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.progress.UIJob;
+import org.xml.sax.SAXException;
 
 import com.piece_framework.makegood.core.launch.IMakeGoodEventListener;
-import com.piece_framework.makegood.launch.elements.TestResultConverter;
+import com.piece_framework.makegood.launch.elements.TestResult;
+import com.piece_framework.makegood.launch.elements.TestResultParser;
+import com.piece_framework.makegood.launch.elements.TestSuite;
 
 public class TestResultViewSetter implements IMakeGoodEventListener {
+    private TestResultParser parser;
+
     @Override
     public void create(ILaunch launch) {
+        String log = null;
+        try {
+            log = launch.getLaunchConfiguration().getAttribute("LOG_JUNIT", (String) null); //$NON-NLS-1$
+        } catch (CoreException e) {
+        }
+        if (log == null) {
+            return;
+        }
+
+        parser = new TestResultParser(new File(log));
         Job job = new UIJob("MakeGood reset") { //$NON-NLS-1$
             @Override
             public IStatus runInUIThread(IProgressMonitor monitor) {
@@ -34,6 +53,19 @@ public class TestResultViewSetter implements IMakeGoodEventListener {
                 view.setFocus();
 
                 view.reset();
+
+                try {
+                    parser.start();
+                } catch (ParserConfigurationException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
 
                 return Status.OK_STATUS;
             }
@@ -53,19 +85,18 @@ public class TestResultViewSetter implements IMakeGoodEventListener {
             }
         }
 
-        String log = null;
-        try {
-            log = launch.getLaunchConfiguration().getAttribute("LOG_JUNIT", (String) null); //$NON-NLS-1$
-        } catch (CoreException e) {
+        parser.terminate();
+        while (!parser.wasEnd() || parser.getTestResults().size() == 0) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+            }
         }
-        if (log == null) {
-            showDebugOutput();
-            return;
-        }
-        final File logFile = new File(log);
-        if (!logFile.exists() || logFile.length() == 0) {
-            showDebugOutput();
-            return;
+        final List<TestSuite> suites = new ArrayList<TestSuite>();
+        for (TestResult result : parser.getTestResults()) {
+            if (result instanceof TestSuite) {
+                suites.add((TestSuite) result);
+            }
         }
 
         Job job = new UIJob("MakeGood result parse") { //$NON-NLS-1$
@@ -77,13 +108,8 @@ public class TestResultViewSetter implements IMakeGoodEventListener {
                     return null;
                 }
                 view.setFocus();
+                view.showTestResult(suites);
 
-                try {
-                    view.showTestResult(TestResultConverter.convert(logFile));
-                } catch (FileNotFoundException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
                 return Status.OK_STATUS;
             }
         };
@@ -104,5 +130,9 @@ public class TestResultViewSetter implements IMakeGoodEventListener {
             }
         };
         job.schedule();
+    }
+
+    private TestResultParser getParser() {
+        return parser;
     }
 }
