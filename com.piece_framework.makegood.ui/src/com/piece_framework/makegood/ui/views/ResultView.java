1c0f1f798185c0d9ffbcc1afe8dc38f7709ed09e
KUBO Atsuhiro <kubo@iteman.jp>
2010-03-31 02:41:33 +0900

    Moved the class TestResultSearch into the class ResultView.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/ResultView.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/ResultView.java
@@ -33,6 +33,7 @@ import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.part.ViewPart;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 import com.piece_framework.makegood.core.runner.ProblemType;
@@ -547,6 +548,79 @@ public class ResultView extends ViewPart {
         public void clearText() {
             setText(""); //$NON-NLS-1$
             hideScrollBar();
+        }
+    }
+
+    private class TestResultSearch {
+        private List<TestResult> results;
+        private TestResult selected;
+        private TestResult findSelected;
+        private TestCase lastFailure;
+
+        public TestResultSearch(List<TestResult> results,
+                                TestResult selected
+                                ) {
+            this.results = results;
+            this.selected = selected;
+        }
+
+        public TestCase getNextFailure() {
+            findSelected = null;
+            return getNextFailure(results);
+        }
+
+        public TestCase getPreviousFailure() {
+            findSelected = null;
+            lastFailure = null;
+            return getPreviousFailure(results);
+        }
+
+        private TestCase getNextFailure(List<TestResult> targets) {
+            for (TestResult result: targets) {
+                if (findSelected == null) {
+                    if (result.getName().equals(selected.getName())) {
+                        findSelected = result;
+                    }
+                } else {
+                    if (result instanceof TestCase
+                        && (result.hasError() || result.hasFailure())
+                        ) {
+                        return (TestCase) result;
+                    }
+                }
+
+                if (result instanceof TestSuite) {
+                    TestCase testCase = getNextFailure(result.getTestResults());
+                    if (testCase != null) {
+                        return testCase;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private TestCase getPreviousFailure(List<TestResult> targets) {
+            for (TestResult result: targets) {
+                if (findSelected == null) {
+                    if (result.getName().equals(selected.getName())) {
+                        return lastFailure;
+                    } else {
+                        if (result instanceof TestCase
+                            && (result.hasError() || result.hasFailure())
+                            ) {
+                            lastFailure = (TestCase) result;
+                        }
+                    }
+                }
+
+                if (result instanceof TestSuite) {
+                    TestCase testCase = getPreviousFailure(result.getTestResults());
+                    if (testCase != null) {
+                        return testCase;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
