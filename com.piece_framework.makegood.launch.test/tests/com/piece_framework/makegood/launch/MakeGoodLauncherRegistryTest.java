fff641669928eb312df1fd3f0aaf658e680c005f
KUBO Atsuhiro <kubo@iteman.jp>
2010-04-14 00:26:42 +0900

    Added copyright notice.

--- com.piece_framework.makegood.launch.test/tests/com/piece_framework/makegood/launch/MakeGoodLauncherRegistryTest.java
+++ com.piece_framework.makegood.launch.test/tests/com/piece_framework/makegood/launch/MakeGoodLauncherRegistryTest.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2009 MATSUFUJI Hideharu <matsufuji2008@gmail.com>,
+ * All rights reserved.
+ *
+ * This file is part of MakeGood.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package com.piece_framework.makegood.launch;
 
 import static org.junit.Assert.assertEquals;
