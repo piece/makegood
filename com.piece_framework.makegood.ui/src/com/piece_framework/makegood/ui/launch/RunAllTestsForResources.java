f7e05aa23dbb969efa3f482e3e82a8a801dbb007
KUBO Atsuhiro <kubo@iteman.jp>
2010-05-22 01:39:58 +0900

    Removed code to avoid opening some views by a launch.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/launch/RunAllTestsForResources.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/launch/RunAllTestsForResources.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2010 MATSUFUJI Hideharu <matsufuji2008@gmail.com>
+ *               2010 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of MakeGood.
@@ -11,8 +12,6 @@
 
 package com.piece_framework.makegood.ui.launch;
 
-import java.io.IOException;
-
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
@@ -20,31 +19,13 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.preferences.InstanceScope;
-import org.eclipse.debug.core.DebugEvent;
-import org.eclipse.debug.core.IDebugEventSetListener;
-import org.eclipse.debug.core.ILaunch;
-import org.eclipse.debug.ui.IDebugUIConstants;
-import org.eclipse.jface.dialogs.MessageDialogWithToggle;
-import org.eclipse.jface.preference.IPersistentPreferenceStore;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.php.internal.debug.core.PHPDebugPlugin;
-import org.eclipse.php.internal.debug.core.preferences.PHPDebugCorePreferenceNames;
-import org.eclipse.ui.preferences.ScopedPreferenceStore;
 import org.eclipse.ui.progress.UIJob;
 
-import com.piece_framework.makegood.launch.MakeGoodLaunchConfigurationDelegate;
 import com.piece_framework.makegood.launch.RuntimeConfiguration;
-import com.piece_framework.makegood.ui.Activator;
 
 public class RunAllTestsForResources implements IResourceChangeListener {
-    private static final String WAIT_FOR_BUILD = IDebugUIConstants.PLUGIN_ID
-                                                 + ".wait_for_build"; //$NON-NLS-1$
-    private String oldValueOfWaitForBuild;
-    private Boolean oldValueOfOpenDebugViews;
-    private Boolean oldValueOfOpenInBrowser;
-
     @Override
     public void resourceChanged(IResourceChangeEvent event) {
         if (!RuntimeConfiguration.getInstance().runsAllTestsWhenFileIsSaved) return;
@@ -56,9 +37,6 @@ public class RunAllTestsForResources implements IResourceChangeListener {
             @Override
             public IStatus runInUIThread(IProgressMonitor monitor) {
                 if (ActivePart.isAllTestsRunnable(selection)) {
-                    restrictProgressMonitor();
-                    restrictDebugView();
-
                     RuntimeConfiguration.getInstance().isRunInBackground = true;
                     TestRunner.runAllTests(selection);
                 }
@@ -66,81 +44,5 @@ public class RunAllTestsForResources implements IResourceChangeListener {
             }
         };
         job.schedule();
-    }
-
-    private void restrictProgressMonitor() {
-        ScopedPreferenceStore store = new ScopedPreferenceStore(
-                                          new InstanceScope(),
-                                          IDebugUIConstants.PLUGIN_ID
-                                      );
-        String waitForBuild = store.getString(WAIT_FOR_BUILD);
-        if (MessageDialogWithToggle.NEVER.equals(waitForBuild)) return;
-
-        store.putValue(WAIT_FOR_BUILD, MessageDialogWithToggle.NEVER);
-        savePreference(store);
-
-        oldValueOfWaitForBuild = waitForBuild;
-    }
-
-    private void restrictDebugView() {
-        ScopedPreferenceStore store = new ScopedPreferenceStore(
-                                          new InstanceScope(),
-                                          PHPDebugPlugin.getID()
-                                      );
-        boolean openDebugViews = store.getBoolean(PHPDebugCorePreferenceNames.OPEN_DEBUG_VIEWS);
-        boolean openInBrowser = store.getBoolean(PHPDebugCorePreferenceNames.OPEN_IN_BROWSER);
-        if (openDebugViews == false && openInBrowser == false) return;
-
-        store.putValue(PHPDebugCorePreferenceNames.OPEN_DEBUG_VIEWS, Boolean.FALSE.toString());
-        store.putValue(PHPDebugCorePreferenceNames.OPEN_IN_BROWSER, Boolean.FALSE.toString());
-        savePreference(store);
-
-        oldValueOfOpenDebugViews = new Boolean(openDebugViews);
-        oldValueOfOpenInBrowser = new Boolean(openInBrowser);
-    }
-
-    private void resetProgressMonitor() {
-        if (oldValueOfWaitForBuild == null) return;
-        ScopedPreferenceStore store = new ScopedPreferenceStore(
-                                          new InstanceScope(),
-                                          IDebugUIConstants.PLUGIN_ID
-                                      );
-        boolean changedByUser = !MessageDialogWithToggle.NEVER.equals(
-                                    store.getString(WAIT_FOR_BUILD)
-                                );
-        if (changedByUser) return;
-
-        store.putValue(WAIT_FOR_BUILD, oldValueOfWaitForBuild);
-        savePreference(store);
-
-        oldValueOfWaitForBuild = null;
-    }
-
-    private void resetDebugView() {
-        if (oldValueOfOpenDebugViews == null
-            && oldValueOfOpenInBrowser == null
-        ) return;
-        ScopedPreferenceStore store = new ScopedPreferenceStore(
-                                          new InstanceScope(),
-                                          PHPDebugPlugin.getID()
-                                      );
-        boolean changedByUser = store.getBoolean(PHPDebugCorePreferenceNames.OPEN_DEBUG_VIEWS) != false
-                                || store.getBoolean(PHPDebugCorePreferenceNames.OPEN_IN_BROWSER) != false;
-        if (changedByUser) return;
-
-        store.putValue(PHPDebugCorePreferenceNames.OPEN_DEBUG_VIEWS, oldValueOfOpenDebugViews.toString());
-        store.putValue(PHPDebugCorePreferenceNames.OPEN_IN_BROWSER, oldValueOfOpenInBrowser.toString());
-        savePreference(store);
-
-        oldValueOfOpenDebugViews = null;
-        oldValueOfOpenInBrowser = null;
-    }
-
-    private void savePreference(IPersistentPreferenceStore store) {
-        try {
-            store.save();
-        } catch (IOException e) {
-            Activator.getDefault().getLog().log(new Status(Status.WARNING, Activator.PLUGIN_ID, e.getMessage(), e));
-        }
     }
 }
