280de37bf99d55ff29fee231ed9cffbbadd18ff6
KUBO Atsuhiro <kubo@iteman.jp>
2010-07-20 20:51:15 +0900

    Removed unused lines.

--- com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/result/TestSuiteResult.java
+++ com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/result/TestSuiteResult.java
@@ -108,16 +108,6 @@ public class TestSuiteResult extends Result {
         return getTestCount() - (getFailureCount() + getErrorCount());
     }
 
-//    void replaceLastChild(Result result) {
-//        if (result == null) {
-//            return;
-//        }
-//
-//        result.parent = this;
-//        children.remove(children.size() - 1);
-//        children.add(result);
-//    }
-
     @Override
     public boolean fixed() {
         for (Result result : children) {
