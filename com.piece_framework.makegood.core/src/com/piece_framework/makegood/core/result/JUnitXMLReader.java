a1841fb05e9b517be9101dbdf1f0f94b91775598
KUBO Atsuhiro <kubo@iteman.jp>
2010-07-22 02:55:43 +0900

    Removed an unused import.

--- com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/result/JUnitXMLReader.java
+++ com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/result/JUnitXMLReader.java
@@ -16,7 +16,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
