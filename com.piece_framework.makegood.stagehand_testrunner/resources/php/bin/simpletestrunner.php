4722461a7495eec75c0846386c6e5c9ef9190cbb
KUBO Atsuhiro <kubo@iteman.jp>
2010-10-05 00:43:55 +0900

    Updated the launcher scripts for Stagehand_TestRunner 2.16.0.

--- com.piece_framework.makegood.stagehand_testrunner/resources/php/bin/simpletestrunner.php
+++ com.piece_framework.makegood.stagehand_testrunner/resources/php/bin/simpletestrunner.php
@@ -32,7 +32,7 @@
  * @package    Stagehand_TestRunner
  * @copyright  2007-2010 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
- * @version    Release: 2.15.0
+ * @version    Release: 2.16.0
  * @since      File available since Release 1.1.0
  */
 
