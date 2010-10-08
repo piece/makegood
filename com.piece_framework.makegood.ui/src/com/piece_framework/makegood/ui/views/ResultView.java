af9cbdb747e7fe5fe5db1372947382f3c973fdcd
KUBO Atsuhiro <kubo@iteman.jp>
2010-10-09 03:46:15 +0900

    Moved the TimeFormatter class into the ResultView class as an inner class.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/ResultView.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/views/ResultView.java
@@ -708,4 +708,23 @@ public class ResultView extends ViewPart {
             }
         }
     }
+
+    private static class TimeFormatter {
+        private static String format(long nanoTime) {
+            double timeForFormat = 0.0d;
+            String unit = null;
+            if (nanoTime >= 1000000000) {
+                timeForFormat = nanoTime / 1000000000d;
+                unit = "s"; //$NON-NLS-1$
+            } else if (nanoTime < 1000000000 && nanoTime >= 1000){
+                timeForFormat = nanoTime / 1000000d;
+                unit = "ms";  //$NON-NLS-1$
+            } else if (nanoTime > 0){
+                return "< 0.001ms"; //$NON-NLS-1$
+            } else {
+                return "0.000ms"; //$NON-NLS-1$
+            }
+            return String.format("%.3f%s", timeForFormat, unit); //$NON-NLS-1$
+        }
+    }
 }
