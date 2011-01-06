daff3870d24634c05a3f245c2692f2e504e74b5e
KUBO Atsuhiro <kubo@iteman.jp>
2011-01-07 03:54:22 +0900

    Fixed indentation.

--- com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/PHPexeItemFactory.java
+++ com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/PHPexeItemFactory.java
@@ -18,7 +18,7 @@ import org.eclipse.php.internal.debug.core.preferences.PHPexeItem;
  * @since 1.2.0
  */
 public class PHPexeItemFactory {
-	public static PHPexeItem create(IProject project) {
-		return null;
-	}
+    public static PHPexeItem create(IProject project) {
+        return null;
+    }
 }
