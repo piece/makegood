c62822f0120a0886d5e428f7889e1d75faf63d3d
KUBO Atsuhiro <kubo@iteman.jp>
2011-01-08 16:31:56 +0900

    Updated copyrights.

--- com.piece_framework.makegood.aspect.org.eclipse.php.debug.core/src/com/piece_framework/makegood/aspect/org/eclipse/php/debug/core/aspect/LaunchWithMissingUserLibrariesFixAspect.java
+++ com.piece_framework.makegood.aspect.org.eclipse.php.debug.core/src/com/piece_framework/makegood/aspect/org/eclipse/php/debug/core/aspect/LaunchWithMissingUserLibrariesFixAspect.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2010-2011 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of MakeGood.
