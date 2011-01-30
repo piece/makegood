7cbcab97a29d86a5cc3f0b336fcd9e64fa5efab5
KUBO Atsuhiro <kubo@iteman.jp>
2011-01-31 00:55:45 +0900

    Added the addTestingTarget() method.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/launch/MakeGoodLaunchShortcut.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/launch/MakeGoodLaunchShortcut.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2009-2010 MATSUFUJI Hideharu <matsufuji2008@gmail.com>,
+ *               2011 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of MakeGood.
@@ -17,6 +18,8 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.php.internal.debug.ui.launching.PHPExeLaunchShortcut;
 import org.eclipse.ui.IEditorPart;
 
+import com.piece_framework.makegood.launch.TestLifecycle;
+
 public class MakeGoodLaunchShortcut extends PHPExeLaunchShortcut {
     @Override
     public void launch(final ISelection selection, final String mode) {
@@ -33,5 +36,12 @@ public class MakeGoodLaunchShortcut extends PHPExeLaunchShortcut {
         return DebugPlugin.getDefault()
                           .getLaunchManager()
                           .getLaunchConfigurationType("com.piece_framework.makegood.launch.makegoodLaunchConfigurationType"); //$NON-NLS-1$
+    }
+
+    /**
+     * @since 1.3.0
+     */
+    protected void addTestingTarget(Object testingTarget) {
+        TestLifecycle.getInstance().getTestingTargets().add(testingTarget);
     }
 }
