9cc8ec981d7bd4ae5052c843b438d952c805efc4
KUBO Atsuhiro <kubo@iteman.jp>
2011-02-16 03:43:11 +0900

    Renamed the class from RunAllTestsWhenFileIsSaved to RunAllTestsWhenFileIsSavedHandler.

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handlers/RunAllTestsWhenFileIsSavedHandler.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/handlers/RunAllTestsWhenFileIsSavedHandler.java
@@ -13,7 +13,7 @@ package com.piece_framework.makegood.ui.handlers;
 
 import com.piece_framework.makegood.ui.actions.RunAllTestsWhenFileIsSavedAction;
 
-public class RunAllTestsWhenFileIsSaved extends AbstractToggleHandler {
+public class RunAllTestsWhenFileIsSavedHandler extends AbstractToggleHandler {
     @Override
     protected String getActionId() {
         return RunAllTestsWhenFileIsSavedAction.ID;
