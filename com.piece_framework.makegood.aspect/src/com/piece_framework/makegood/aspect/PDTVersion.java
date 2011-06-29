f94e33cab81d34320bf41cc7b2776652b25cda51
KUBO Atsuhiro <kubo@iteman.jp>
2011-06-30 00:31:23 +0900

    Replaced the required version number string with a constant.

--- com.piece_framework.makegood.aspect/src/com/piece_framework/makegood/aspect/PDTVersion.java
+++ com.piece_framework.makegood.aspect/src/com/piece_framework/makegood/aspect/PDTVersion.java
@@ -20,6 +20,7 @@ import org.osgi.framework.Version;
  * @since 1.2.0
  */
 public class PDTVersion {
+    private static final String REQUIRED_VERSION = "2.1.0"; //$NON-NLS-1$
     private static PDTVersion soleInstance;
     private Version version;
 
@@ -28,8 +29,8 @@ public class PDTVersion {
         Assert.isNotNull(bundle, "No bundle is found for org.eclipse.php.core."); //$NON-NLS-1$
         Version version = bundle.getVersion();
         Assert.isTrue(
-            version.compareTo(Version.parseVersion("2.1.0")) >= 0, //$NON-NLS-1$
-            "The version of the bundle org.eclipse.php.core must be greater than or equal to 2.1.0." //$NON-NLS-1$
+            version.compareTo(Version.parseVersion(REQUIRED_VERSION)) >= 0,
+            "The version of the bundle org.eclipse.php.core must be greater than or equal to " + REQUIRED_VERSION + "." //$NON-NLS-1$ //$NON-NLS-2$
         );
         this.version = version;
     }
