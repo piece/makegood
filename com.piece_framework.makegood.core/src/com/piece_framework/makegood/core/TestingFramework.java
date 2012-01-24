878cf18e5f29a224cd5f1a06c9f3752ea9f28235
KUBO Atsuhiro <kubo@iteman.jp>
2012-01-24 14:21:24 +0900

    Update copyrights.

--- com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/TestingFramework.java
+++ com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/TestingFramework.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2009 MATSUFUJI Hideharu <matsufuji2008@gmail.com>,
- *               2011 KUBO Atsuhiro <kubo@iteman.jp>,
+ *               2011-2012 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of MakeGood.
