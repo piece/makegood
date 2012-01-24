ab448d979a8a3ac3f8129e79c8fa3ac87b9f3fe1
KUBO Atsuhiro <kubo@iteman.jp>
2012-01-24 13:37:03 +0900

    Changed the filter object for the preload script file selection from PHPResourceViewerFilter to FileViewerFilter not to filter by file extension. (Issue #295)

--- com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/propertyPages/MakeGoodPropertyPage.java
+++ com.piece_framework.makegood.ui/src/com/piece_framework/makegood/ui/propertyPages/MakeGoodPropertyPage.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2009-2010 MATSUFUJI Hideharu <matsufuji2008@gmail.com>,
- *               2010-2011 KUBO Atsuhiro <kubo@iteman.jp>,
+ *               2010-2012 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of MakeGood.
@@ -279,7 +279,7 @@ public class MakeGoodPropertyPage extends PropertyPage {
                 Messages.MakeGoodPropertyPage_preloadScriptDialogTitle,
                 Messages.MakeGoodPropertyPage_preloadScriptDialogMessage,
                 SELECTION_ALLOW_FILE,
-                new PHPResourceViewerFilter()
+                new FileViewerFilter()
             )
         );
 
