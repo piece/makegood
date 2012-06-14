1124e3428e96cc8f0379191478c8f37a924d86b2
KUBO Atsuhiro <kubo@iteman.jp>
2012-06-14 15:59:44 +0900

    [aspect.org.eclipse.php.core] Added support for multibyte characters for PHP 5.4. (Issue #412)

--- com.piece_framework.makegood.aspect.org.eclipse.php.core/src/com/piece_framework/makegood/aspect/org/eclipse/php/core/aspect/MultibyteCharactersAspect.java
+++ com.piece_framework.makegood.aspect.org.eclipse.php.core/src/com/piece_framework/makegood/aspect/org/eclipse/php/core/aspect/MultibyteCharactersAspect.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2011 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2010-2012 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of MakeGood.
@@ -26,26 +26,50 @@ import com.piece_framework.makegood.aspect.Aspect;
  * @since 1.2.0
  */
 public class MultibyteCharactersAspect extends Aspect {
+    /**
+     * @since 2.1.0
+     */
+    private static final String JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP54 = "PhpAstLexer#next_token [access CMAP php54]"; //$NON-NLS-1$
+
     private static final String JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP53 = "PhpAstLexer#next_token [access CMAP php53]"; //$NON-NLS-1$
     private static final String JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP5 = "PhpAstLexer#next_token [access CMAP php5]"; //$NON-NLS-1$
     private static final String JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP4 = "PhpAstLexer#next_token [access CMAP php4]"; //$NON-NLS-1$
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP54 = "PhpLexer#yylex [access CMAP php54]"; //$NON-NLS-1$
     private static final String JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP53 = "PhpLexer#yylex [access CMAP php53]"; //$NON-NLS-1$
     private static final String JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP5 = "PhpLexer#yylex [access CMAP php5]"; //$NON-NLS-1$
     private static final String JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP4 = "PhpLexer#yylex [access CMAP php4]"; //$NON-NLS-1$
     private static final String[] JOINPOINTS = {
+        JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP54,
         JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP53,
         JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP5,
         JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP4,
+        JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP54,
         JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP53,
         JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP5,
         JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP4
     };
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String WEAVINGCLASS_PHPASTLEXER_PHP54 =
+        "org.eclipse.php.internal.core.ast.scanner.php54.PhpAstLexer"; //$NON-NLS-1$
     private static final String WEAVINGCLASS_PHPASTLEXER_PHP53 =
         "org.eclipse.php.internal.core.ast.scanner.php53.PhpAstLexer"; //$NON-NLS-1$
     private static final String WEAVINGCLASS_PHPASTLEXER_PHP5 =
         "org.eclipse.php.internal.core.ast.scanner.php5.PhpAstLexer"; //$NON-NLS-1$
     private static final String WEAVINGCLASS_PHPASTLEXER_PHP4 =
         "org.eclipse.php.internal.core.ast.scanner.php4.PhpAstLexer"; //$NON-NLS-1$
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String WEAVINGCLASS_PHPLEXER_PHP54 =
+        "org.eclipse.php.internal.core.documentModel.parser.php54.PhpLexer"; //$NON-NLS-1$
     private static final String WEAVINGCLASS_PHPLEXER_PHP53 =
         "org.eclipse.php.internal.core.documentModel.parser.php53.PhpLexer"; //$NON-NLS-1$
     private static final String WEAVINGCLASS_PHPLEXER_PHP5 =
@@ -53,13 +77,49 @@ public class MultibyteCharactersAspect extends Aspect {
     private static final String WEAVINGCLASS_PHPLEXER_PHP4 =
         "org.eclipse.php.internal.core.documentModel.parser.php4.PhpLexer"; //$NON-NLS-1$
     private static final String[] WEAVINGCLASSES = {
+        WEAVINGCLASS_PHPASTLEXER_PHP54,
         WEAVINGCLASS_PHPASTLEXER_PHP53,
         WEAVINGCLASS_PHPASTLEXER_PHP5,
         WEAVINGCLASS_PHPASTLEXER_PHP4,
+        WEAVINGCLASS_PHPLEXER_PHP54,
         WEAVINGCLASS_PHPLEXER_PHP53,
         WEAVINGCLASS_PHPLEXER_PHP5,
         WEAVINGCLASS_PHPLEXER_PHP4
     };
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String PHPASTLEXER_FIELD_CMAP_PACKED_PHP54 =
+"private static final String ZZ_CMAP_PACKED_MULTIBYTECHARACTERS = \"\\\11\\\0\\\1\\\14\\\1\\\13\\\2\\\0\\\1\\\15\\\22\\\0\\\1\\\14\\\1\\\60\\\1\\\17\\\1\\\101\\\1\\\16\\\1\\\64\\\1\\\65\\\1\\\100\\\1\\\52\\\1\\\53\\\1\\\62\\\1\\\4\\\1\\\70\\\1\\\46\\\1\\\2\\\1\\\63\\\1\\\5\\\1\\\11\\\10\\\1\\\1\\\50\\\1\\\23\\\1\\\61\\\1\\\57\\\1\\\47\\\1\\\74\\\1\\\75\\\1\\\7\\\1\\\10\\\1\\\32\\\1\\\26\\\1\\\3\\\1\\\27\\\1\\\43\\\1\\\37\\\1\\\24\\\1\\\54\\\1\\\42\\\1\\\41\\\1\\\45\\\1\\\31\\\1\\\33\\\1\\\44\\\1\\\56\\\1\\\35\\\1\\\34\\\1\\\25\\\1\\\30\\\1\\\51\\\1\\\40\\\1\\\6\\\1\\\36\\\1\\\12\\\1\\\71\\\1\\\20\\\1\\\72\\\1\\\67\\\1\\\55\\\1\\\21\\\1\\\7\\\1\\\10\\\1\\\32\\\1\\\26\\\1\\\3\\\1\\\27\\\1\\\43\\\1\\\37\\\1\\\24\\\1\\\54\\\1\\\42\\\1\\\41\\\1\\\45\\\1\\\31\\\1\\\33\\\1\\\44\\\1\\\56\\\1\\\35\\\1\\\77\\\1\\\25\\\1\\\30\\\1\\\51\\\1\\\40\\\1\\\6\\\1\\\36\\\1\\\12\\\1\\\22\\\1\\\66\\\1\\\76\\\1\\\73\\\uff81\\\12\";"; //$NON-NLS-1$
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String PHPASTLEXER_METHOD_UNPACKCMAP_PHP54 = generateZZUnpackCMapMethod(190);
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String PHPASTLEXER_FIELD_CMAP_PHP54 =
+"private static final char [] ZZ_CMAP_MULTIBYTECHARACTERS = zzUnpackCMapMultibyteCharacters(ZZ_CMAP_PACKED_MULTIBYTECHARACTERS);"; //$NON-NLS-1$
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String PHPLEXER_FIELD_CMAP_PACKED_PHP54 =
+"private static final String ZZ_CMAP_PACKED_MULTIBYTECHARACTERS = \"\\\11\\\0\\\1\\\14\\\1\\\13\\\2\\\0\\\1\\\17\\\22\\\0\\\1\\\14\\\1\\\30\\\1\\\21\\\1\\\75\\\1\\\20\\\1\\\34\\\1\\\35\\\1\\\74\\\1\\\64\\\1\\\65\\\1\\\32\\\1\\\27\\\1\\\15\\\1\\\4\\\1\\\2\\\1\\\33\\\1\\\5\\\1\\\11\\\10\\\1\\\1\\\62\\\1\\\16\\\1\\\31\\\1\\\25\\\1\\\26\\\1\\\76\\\1\\\77\\\1\\\42\\\1\\\10\\\1\\\7\\\1\\\44\\\1\\\3\\\1\\\47\\\1\\\57\\\1\\\53\\\1\\\45\\\1\\\66\\\1\\\56\\\1\\\55\\\1\\\61\\\1\\\43\\\1\\\40\\\1\\\60\\\1\\\70\\\1\\\41\\\1\\\51\\\1\\\46\\\1\\\50\\\1\\\63\\\1\\\54\\\1\\\6\\\1\\\52\\\1\\\12\\\1\\\72\\\1\\\22\\\1\\\73\\\1\\\37\\\1\\\67\\\1\\\23\\\1\\\42\\\1\\\10\\\1\\\7\\\1\\\44\\\1\\\3\\\1\\\47\\\1\\\57\\\1\\\53\\\1\\\45\\\1\\\66\\\1\\\56\\\1\\\55\\\1\\\61\\\1\\\43\\\1\\\40\\\1\\\60\\\1\\\70\\\1\\\41\\\1\\\51\\\1\\\46\\\1\\\50\\\1\\\63\\\1\\\54\\\1\\\6\\\1\\\52\\\1\\\12\\\1\\\24\\\1\\\36\\\1\\\71\\\1\\\15\\\uff81\\\12\";"; //$NON-NLS-1$
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String PHPLEXER_METHOD_UNPACKCMAP_PHP54 = PHPASTLEXER_METHOD_UNPACKCMAP_PHP54;
+
+    /**
+     * @since 2.1.0
+     */
+    private static final String PHPLEXER_FIELD_CMAP_PHP54 = PHPASTLEXER_FIELD_CMAP_PHP54;
+
     private static final String PHPASTLEXER_FIELD_CMAP_PACKED_PHP53 =
 "private static final String ZZ_CMAP_PACKED_MULTIBYTECHARACTERS = \"\\\11\\\0\\\1\\\12\\\1\\\11\\\2\\\0\\\1\\\13\\\22\\\0\\\1\\\12\\\1\\\57\\\1\\\15\\\1\\\100\\\1\\\14\\\1\\\63\\\1\\\64\\\1\\\77\\\1\\\51\\\1\\\52\\\1\\\61\\\1\\\4\\\1\\\67\\\1\\\45\\\1\\\2\\\1\\\62\\\1\\\5\\\11\\\1\\\1\\\47\\\1\\\21\\\1\\\60\\\1\\\56\\\1\\\46\\\1\\\73\\\1\\\74\\\1\\\35\\\1\\\7\\\1\\\30\\\1\\\24\\\1\\\3\\\1\\\25\\\1\\\42\\\1\\\36\\\1\\\22\\\1\\\53\\\1\\\41\\\1\\\40\\\1\\\44\\\1\\\27\\\1\\\31\\\1\\\43\\\1\\\55\\\1\\\33\\\1\\\32\\\1\\\23\\\1\\\26\\\1\\\50\\\1\\\37\\\1\\\6\\\1\\\34\\\1\\\10\\\1\\\70\\\1\\\16\\\1\\\71\\\1\\\66\\\1\\\54\\\1\\\17\\\1\\\35\\\1\\\7\\\1\\\30\\\1\\\24\\\1\\\3\\\1\\\25\\\1\\\42\\\1\\\36\\\1\\\22\\\1\\\53\\\1\\\41\\\1\\\40\\\1\\\44\\\1\\\27\\\1\\\31\\\1\\\43\\\1\\\55\\\1\\\33\\\1\\\76\\\1\\\23\\\1\\\26\\\1\\\50\\\1\\\37\\\1\\\6\\\1\\\34\\\1\\\10\\\1\\\20\\\1\\\65\\\1\\\75\\\1\\\72\\\uff81\\\10\";"; //$NON-NLS-1$
     private static final String PHPASTLEXER_METHOD_UNPACKCMAP_PHP53 = generateZZUnpackCMapMethod(188);
@@ -92,6 +152,7 @@ public class MultibyteCharactersAspect extends Aspect {
 
     @Override
     protected void doWeave() throws NotFoundException, CannotCompileException {
+        doWeavePHP54();
         doWeavePHP53();
         doWeavePHP5();
         doWeavePHP4();
@@ -105,6 +166,43 @@ public class MultibyteCharactersAspect extends Aspect {
     @Override
     protected String[] weavingClasses() {
         return WEAVINGCLASSES;
+    }
+
+    /**
+     * @since 2.1.0
+     */
+    private void doWeavePHP54() throws NotFoundException, CannotCompileException {
+        CtClass phpAstLexerClass = ClassPool.getDefault().get(WEAVINGCLASS_PHPASTLEXER_PHP54);
+        phpAstLexerClass.addField(CtField.make(PHPASTLEXER_FIELD_CMAP_PACKED_PHP54, phpAstLexerClass));
+        phpAstLexerClass.addMethod(CtNewMethod.make(PHPASTLEXER_METHOD_UNPACKCMAP_PHP54, phpAstLexerClass));
+        phpAstLexerClass.addField(CtField.make(PHPASTLEXER_FIELD_CMAP_PHP54, phpAstLexerClass));
+        phpAstLexerClass.getDeclaredMethod("next_token").instrument( //$NON-NLS-1$
+            new ExprEditor() {
+                public void edit(FieldAccess fieldAccess) throws CannotCompileException {
+                    if ("ZZ_CMAP".equals(fieldAccess.getFieldName())) { //$NON-NLS-1$
+                        fieldAccess.replace("$_ = ZZ_CMAP_MULTIBYTECHARACTERS;"); //$NON-NLS-1$
+                        markJoinPointAsPassed(JOINPOINT_PHPASTLEXER_ACCESS_CMAP_PHP54);
+                    }
+                }
+            }
+        );
+        markClassAsWoven(phpAstLexerClass);
+
+        CtClass phpLexerClass = ClassPool.getDefault().get(WEAVINGCLASS_PHPLEXER_PHP54);
+        phpLexerClass.addField(CtField.make(PHPLEXER_FIELD_CMAP_PACKED_PHP54, phpLexerClass));
+        phpLexerClass.addMethod(CtNewMethod.make(PHPLEXER_METHOD_UNPACKCMAP_PHP54, phpLexerClass));
+        phpLexerClass.addField(CtField.make(PHPLEXER_FIELD_CMAP_PHP54, phpLexerClass));
+        phpLexerClass.getDeclaredMethod("yylex").instrument( //$NON-NLS-1$
+            new ExprEditor() {
+                public void edit(FieldAccess fieldAccess) throws CannotCompileException {
+                    if ("ZZ_CMAP".equals(fieldAccess.getFieldName())) { //$NON-NLS-1$
+                        fieldAccess.replace("$_ = ZZ_CMAP_MULTIBYTECHARACTERS;"); //$NON-NLS-1$
+                        markJoinPointAsPassed(JOINPOINT_PHPLEXER_ACCESS_CMAP_PHP54);
+                    }
+                }
+            }
+        );
+        markClassAsWoven(phpLexerClass);
     }
 
     private void doWeavePHP53() throws NotFoundException, CannotCompileException {
