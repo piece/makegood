56753404b146f424036f63963022fd347b00730c
KUBO Atsuhiro <kubo@iteman.jp>
2012-06-22 23:47:50 +0900

    [aspect.org.eclipse.php.core] Removed an unused import.

--- com.piece_framework.makegood.aspect.org.eclipse.php.core/src/com/piece_framework/makegood/aspect/org/eclipse/php/core/aspect/SystemIncludePathAspect.java
+++ com.piece_framework.makegood.aspect.org.eclipse.php.core/src/com/piece_framework/makegood/aspect/org/eclipse/php/core/aspect/SystemIncludePathAspect.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2009-2010 MATSUFUJI Hideharu <matsufuji2008@gmail.com>,
- *               2010-2011 KUBO Atsuhiro <kubo@iteman.jp>,
+ *               2010-2012 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of MakeGood.
@@ -15,7 +15,6 @@ package com.piece_framework.makegood.aspect.org.eclipse.php.core.aspect;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
-import javassist.CtMethod;
 import javassist.NotFoundException;
 import javassist.expr.Cast;
 import javassist.expr.ExprEditor;
