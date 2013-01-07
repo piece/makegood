c56e314556154ab7ae70a4a93de88723dc29e335
KUBO Atsuhiro <kubo@iteman.jp>
2013-01-07 17:10:50 +0900

    [core] Changed the MakeGoodPreferenceInitializer class to enable the continuous testing feature by default.

--- com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/preference/MakeGoodPreferenceInitializer.java
+++ com.piece_framework.makegood.core/src/com/piece_framework/makegood/core/preference/MakeGoodPreferenceInitializer.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2010 MATSUFUJI Hideharu <matsufuji2008@gmail.com>,
- *               2011-2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ *               2011-2013 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * This file is part of MakeGood.
@@ -20,6 +20,7 @@ import com.piece_framework.makegood.core.DefaultConfiguration;
 public class MakeGoodPreferenceInitializer extends AbstractPreferenceInitializer {
     @Override
     public void initializeDefaultPreferences() {
+        Activator.getDefault().getPreferenceStore().setDefault(MakeGoodPreference.CONTINUOUS_TESTING_ENABLED, new DefaultConfiguration().getAutotestEnabled());
         Activator.getDefault().getPreferenceStore().setDefault(MakeGoodPreference.CONTINUOUS_TESTING_SCOPE, new DefaultConfiguration().getAutotestScope().name());
     }
 }
