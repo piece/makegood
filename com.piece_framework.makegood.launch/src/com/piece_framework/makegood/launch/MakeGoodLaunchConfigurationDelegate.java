728f3c6a165c5b0402f8ab5c1764133135036f76
KUBO Atsuhiro <kubo@iteman.jp>
2013-10-30 00:38:42 +0900

    [launch] introduced the CommandLineBuilder class to build the command line for a test run

--- com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/MakeGoodLaunchConfigurationDelegate.java
+++ com.piece_framework.makegood.launch/src/com/piece_framework/makegood/launch/MakeGoodLaunchConfigurationDelegate.java
@@ -13,16 +13,9 @@
 package com.piece_framework.makegood.launch;
 
 import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.HashSet;
-import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -38,12 +31,7 @@ import org.eclipse.debug.core.model.ILaunchConfigurationDelegate2;
 import org.eclipse.debug.internal.core.LaunchConfiguration;
 import org.eclipse.debug.internal.core.LaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.DebugUITools;
-import org.eclipse.dltk.core.IMethod;
-import org.eclipse.dltk.core.IModelElement;
-import org.eclipse.dltk.core.IType;
-import org.eclipse.osgi.framework.debug.Debug;
 import org.eclipse.php.debug.core.debugger.parameters.IDebugParametersKeys;
-import org.eclipse.php.internal.core.typeinference.PHPClassType;
 import org.eclipse.php.internal.debug.core.IPHPDebugConstants;
 import org.eclipse.php.internal.debug.core.PHPDebugPlugin;
 import org.eclipse.php.internal.debug.core.launching.PHPLaunchDelegateProxy;
@@ -52,9 +40,6 @@ import org.eclipse.php.internal.debug.core.preferences.PHPDebuggersRegistry;
 import org.eclipse.php.internal.debug.core.preferences.PHPProjectPreferences;
 import org.eclipse.php.internal.debug.core.preferences.PHPexeItem;
 
-import com.piece_framework.makegood.core.PHPType;
-import com.piece_framework.makegood.core.TestingFramework;
-import com.piece_framework.makegood.core.preference.MakeGoodProperty;
 import com.piece_framework.makegood.stagehandtestrunner.StagehandTestRunner;
 
 public class MakeGoodLaunchConfigurationDelegate extends PHPLaunchDelegateProxy {
@@ -272,7 +257,7 @@ public class MakeGoodLaunchConfigurationDelegate extends PHPLaunchDelegateProxy 
         workingCopy.setAttribute(MAKEGOOD_JUNIT_XML_FILE, junitXMLFile);
         workingCopy.setAttribute(
             IDebugParametersKeys.EXE_CONFIG_PROGRAM_ARGUMENTS,
-            generateCommandLine(junitXMLFile)
+            new CommandLineBuilder(junitXMLFile).build()
         );
 
         IProject project = TestLifecycle.getInstance().getTestTargets().getProject();
@@ -311,214 +296,5 @@ public class MakeGoodLaunchConfigurationDelegate extends PHPLaunchDelegateProxy 
     private void cancelLaunch(IProgressMonitor monitor) {
         monitor.setCanceled(true);
         cancelLaunch();
-    }
-
-    /**
-     * @since 2.5.0
-     */
-    private String generateCommandLine(String junitXMLFile) throws CoreException, MethodNotFoundException, ResourceNotFoundException {
-        Assert.isNotNull(junitXMLFile, "The JUnit XML file should not be null."); //$NON-NLS-1$
-
-        MakeGoodProperty property = new MakeGoodProperty(TestLifecycle.getInstance().getTestTargets().getFirstResource());
-        StringBuilder buffer = new StringBuilder();
-
-        buffer.append(" --no-ansi"); //$NON-NLS-1$
-        buffer.append(" " + property.getTestingFramework().name().toLowerCase()); //$NON-NLS-1$
-
-        String preloadScript = property.getPreloadScript();
-        if (!preloadScript.equals("")) { //$NON-NLS-1$
-            IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-            IResource preloadResource = root.findMember(preloadScript);
-            if (preloadResource == null) {
-                throw new ResourceNotFoundException("The resource [ " + preloadScript + " ] is not found."); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-
-            buffer.append(" -p \"" + preloadResource.getLocation().toString() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-        }
-
-        buffer.append(" --log-junit=\"" + junitXMLFile + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-        buffer.append(" --log-junit-realtime"); //$NON-NLS-1$
-
-        if (RuntimeConfiguration.getInstance().stopsOnFailure) {
-            buffer.append(" -s"); //$NON-NLS-1$
-        }
-
-        if (property.getTestingFramework() == TestingFramework.PHPUnit) {
-            String phpunitConfigFile = property.getPHPUnitConfigFile();
-            if (!"".equals(phpunitConfigFile)) { //$NON-NLS-1$
-                IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(phpunitConfigFile);
-                if (resource == null) {
-                    throw new ResourceNotFoundException("The resource [ " + phpunitConfigFile + " ] is not found."); //$NON-NLS-1$ //$NON-NLS-2$
-                }
-
-                buffer.append(" --phpunit-config=\"" + resource.getLocation().toString() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-        } else if (property.getTestingFramework() == TestingFramework.CakePHP) {
-            String cakephpAppPath = property.getCakePHPAppPath();
-            if ("".equals(cakephpAppPath)) { //$NON-NLS-1$
-                cakephpAppPath = getDefaultCakePHPAppPath();
-            }
-            if (!"".equals(cakephpAppPath)) { //$NON-NLS-1$
-                IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(cakephpAppPath);
-                if (resource == null) {
-                    throw new ResourceNotFoundException("The resource [ " + cakephpAppPath + " ] is not found."); //$NON-NLS-1$ //$NON-NLS-2$
-                }
-
-                buffer.append(" --cakephp-app-path=\"" + resource.getLocation().toString() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-
-            String cakephpCorePath = property.getCakePHPCorePath();
-            if (!"".equals(cakephpCorePath)) { //$NON-NLS-1$
-                IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(cakephpCorePath);
-                if (resource == null) {
-                    throw new ResourceNotFoundException("The resource [ " + cakephpCorePath + " ] is not found."); //$NON-NLS-1$ //$NON-NLS-2$
-                }
-
-                buffer.append(" --cakephp-core-path=\"" + resource.getLocation().toString() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-        } else if (property.getTestingFramework() == TestingFramework.CIUnit) {
-            String ciunitPath = property.getCIUnitPath();
-            if ("".equals(ciunitPath)) { //$NON-NLS-1$
-                ciunitPath = getDefaultCIUnitPath();
-            }
-            if (!"".equals(ciunitPath)) { //$NON-NLS-1$
-                IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(ciunitPath);
-                if (resource == null) {
-                    throw new ResourceNotFoundException("The resource [ " + ciunitPath + " ] is not found."); //$NON-NLS-1$ //$NON-NLS-2$
-                }
-
-                buffer.append(" --ciunit-path=\"" + resource.getLocation().toString() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-
-            String ciunitConfigFile = property.getCIUnitConfigFile();
-            if (!"".equals(ciunitConfigFile)) { //$NON-NLS-1$
-                IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(ciunitConfigFile);
-                if (resource == null) {
-                    throw new ResourceNotFoundException("The resource [ " + ciunitConfigFile + " ] is not found."); //$NON-NLS-1$ //$NON-NLS-2$
-                }
-
-                buffer.append(" --phpunit-config=\"" + resource.getLocation().toString() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-        }
-
-        Set<String> testFiles = new HashSet<String>();
-        Set<String> testClasses = new HashSet<String>();
-        Set<String> testMethods = new HashSet<String>();
-        for (Object testTarget: TestLifecycle.getInstance().getTestTargets().getAll()) {
-            IResource resource = TestLifecycle.getInstance().getTestTargets().getResource(testTarget);
-            if (resource == null || resource.exists() == false) {
-                throw new ResourceNotFoundException("The resource for the test target [ " + testTarget + " ] is not found."); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-
-            testFiles.add(resource.getLocation().toString());
-            if (testTarget instanceof IType) {
-                PHPType phpType = new PHPType((IType) testTarget, property.getTestingFramework());
-                if (phpType.isNamespace()) {
-                    for (IType type: ((IType) testTarget).getTypes()) {
-                        testClasses.add(urlencode(PHPClassType.fromIType(type).getTypeName()));
-                    }
-                } else if (phpType.isClass()) {
-                    testClasses.add(urlencode(PHPClassType.fromIType((IType) testTarget).getTypeName()));
-                }
-            } else if (testTarget instanceof IMethod) {
-                IMethod method = findMethod((IMethod) testTarget);
-                if (method == null) {
-                    throw new MethodNotFoundException("An unknown method context [ " + testTarget + " ] has been found."); //$NON-NLS-1$ //$NON-NLS-2$
-                }
-                testMethods.add(
-                    urlencode(
-                        PHPClassType.fromIType(method.getDeclaringType()).getTypeName() +
-                        "::" + //$NON-NLS-1$
-                        method.getElementName()
-                    )
-                );
-            } else if (testTarget instanceof ClassTestTarget) {
-                testClasses.add(urlencode(((ClassTestTarget) testTarget).getClassName()));
-            }
-        }
-
-        if (testClasses.size() > 0) {
-            for (String testClass: testClasses) {
-                buffer.append(" --test-class=\"" + testClass.toString() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-        }
-
-        if (testMethods.size() > 0) {
-            for (String testMethod: testMethods) {
-                buffer.append(" --test-method=\"" + testMethod.toString() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-        }
-
-        buffer.append(" -R"); //$NON-NLS-1$
-        buffer.append(
-            " --test-file-pattern=\"" + //$NON-NLS-1$
-            (property.getTestFilePattern().equals("") ? property.getTestingFramework().getTestFilePattern() : property.getTestFilePattern()) + //$NON-NLS-1$
-            "\"" //$NON-NLS-1$
-        );
-        for (String testFile: testFiles) {
-            buffer.append(" \"" + testFile + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-        }
-
-        Debug.println(buffer.toString());
-
-        return buffer.toString();
-    }
-
-    /**
-     * @since 2.5.0
-     */
-    private String getDefaultCakePHPAppPath() {
-        Assert.isNotNull(TestLifecycle.getInstance().getTestTargets().getProject(), "One or more test targets should be added."); //$NON-NLS-1$
-
-        IResource resource = TestLifecycle.getInstance().getTestTargets().getProject().findMember("/app"); //$NON-NLS-1$
-        if (resource == null) return ""; //$NON-NLS-1$
-
-        return resource.getFullPath().toString();
-    }
-
-    /**
-     * @since 2.5.0
-     */
-    private String getDefaultCIUnitPath() {
-        Assert.isNotNull(TestLifecycle.getInstance().getTestTargets().getProject(), "One or more test targets should be added."); //$NON-NLS-1$
-
-        IResource resource = TestLifecycle.getInstance().getTestTargets().getProject().findMember("/system/application/tests"); //$NON-NLS-1$
-        if (resource == null) return ""; //$NON-NLS-1$
-
-        return resource.getFullPath().toString();
-    }
-
-    /**
-     * @since 2.5.0
-     */
-    private String urlencode(String subject) throws CoreException
-    {
-        try {
-            return URLEncoder.encode(subject, TestLifecycle.getInstance().getTestTargets().getProject().getDefaultCharset());
-        } catch (UnsupportedEncodingException e) {
-            Activator.getDefault().getLog().log(new Status(Status.WARNING, Activator.PLUGIN_ID, e.getMessage(), e));
-
-            return subject;
-        }
-    }
-
-    /**
-     * @since 2.5.0
-     */
-    private IMethod findMethod(IMethod method) {
-        IModelElement parent = method.getParent();
-        if (parent == null) return null;
-        if (parent instanceof IType) {
-            return method;
-        }
-
-        while (true) {
-            if (parent instanceof IMethod) {
-                return findMethod((IMethod) parent);
-            }
-
-            parent = parent.getParent();
-            if (parent == null) return null;
-        }
     }
 }
